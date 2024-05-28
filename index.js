d89609d97e7ba18505835d59c9ed96cfa2b2ca22
Rana Hari <ranahari3@gmail.com>
2024-05-28 21:45:37 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login": "Rana628",
+  "id": 121398488,
+  "node_id": "U_kgDOBzxk2A",
+  "avatar_url": "https://avatars.githubusercontent.com/u/121398488?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Rana628",
+  "html_url": "https://github.com/Rana628",
+  "followers_url": "https://api.github.com/users/Rana628/followers",
+  "following_url": "https://api.github.com/users/Rana628/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Rana628/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Rana628/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Rana628/subscriptions",
+  "organizations_url": "https://api.github.com/users/Rana628/orgs",
+  "repos_url": "https://api.github.com/users/Rana628/repos",
+  "events_url": "https://api.github.com/users/Rana628/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Rana628/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 1,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2022-12-25T17:16:31Z",
+  "updated_at": "2022-12-25T17:16:31Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -15,6 +50,10 @@ app.get('/login', (req, res)=> {
 })
 app.get('/youtube', (req,res)=>{
     res.send("<h2> Chai aur Code</h2>")
+})
+
+app.get('/github', (req, res)=> {
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
